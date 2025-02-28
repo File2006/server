7d56d0d86ee9d28e8bc4a150a64bd941ad5a91e0
file06 <file2006@seznam.cz>
2025-02-28 20:19:44 +0100

    Working version before remote cam

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
-var indexRouter = require('./routes/index');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const indexRouter = require('./routes/index');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const path = require('path');
+const favicon = require('serve-favicon');
 
-var app = express();
 
-app.use(cors());
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: 'Too many requests, please try again later.'
+});
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(helmet());
+
+app.use(limiter);
+
+app.use(cors());
+
+app.use(logger('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+
 app.use('/', indexRouter);
-
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-    res.json({ message: 'Welcome to the Express API!' });
+const express = require('express');
+const router = express.Router();
+router.get('/api/test', (req, res) => {
+    res.json({ message: 'Backend is connected successfully' });
 });
-
+router.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
 module.exports = router;
