763fea57cc9566673a21f1a0bb0ccac6b37e3ce0
file06 <file2006@seznam.cz>
2025-03-02 22:35:47 +0100

    Working remote cam with no algorithms

--- app.js
+++ app.js
@@ -16,23 +16,16 @@ const limiter = rateLimit({
 });
 
 const app = express();
+
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
 app.use(helmet());
-
 app.use(limiter);
-
 app.use(cors());
-
 app.use(logger('dev'));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use(cookieParser());
-
-app.use('/', indexRouter);
+app.use('/api', indexRouter);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
-router.get('/api/test', (req, res) => {
-    res.json({ message: 'Backend is connected successfully' });
+let idList = [];
+
+router.post('/registerPeer', (req, res) => {
+    const { peerID } = req.body;
+    if (!peerID) {
+        return res.status(400).json({ error: 'Peer ID is required' });
+    }
+
+    idList.push(peerID);
+    console.log(`Peer ID ${peerID} registered. Current list of peers:`, idList);
+
+    res.json({ message: 'Peer registered successfully' });
 });
-router.get('/', (req, res) => {
-    res.send('Hello, World!');
+
+router.get('/getPeers', (req, res) => {
+    res.json({ idList });
 });
+
 module.exports = router;
