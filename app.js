7938805f87cdd0892b6514c4c998d56dc121a471
file06 <file2006@seznam.cz>
2025-03-10 01:05:01 +0100

    Working remote cam with almost functional algorithms

--- app.js
+++ app.js
@@ -4,23 +4,15 @@ const logger = require('morgan');
 const cors = require('cors');
 const indexRouter = require('./routes/index');
 const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
 const path = require('path');
 const favicon = require('serve-favicon');
 
-
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 100,
-    message: 'Too many requests, please try again later.'
-});
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(helmet());
-app.use(limiter);
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,20 @@ router.post('/registerPeer', (req, res) => {
     if (!peerID) {
         return res.status(400).json({ error: 'Peer ID is required' });
     }
-
     idList.push(peerID);
     console.log(`Peer ID ${peerID} registered. Current list of peers:`, idList);
 
     res.json({ message: 'Peer registered successfully' });
+});
+router.post('/destroyPeer', (req, res) => {
+    const { peerID } = req.body;
+    if (!peerID) {
+        return res.status(400).json({ error: 'Peer ID is required' });
+    }
+    idList = idList.filter(id => id !== peerID)
+    console.log(`Peer ID ${peerID} destroyed. Current list of peers:`, idList);
+
+    res.json({ message: 'Peer destroyed successfully' });
 });
 
 router.get('/getPeers', (req, res) => {
