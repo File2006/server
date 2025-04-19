e7655eeca1db62de6c7211ff7758828be691a666
file06 <file2006@seznam.cz>
2025-04-19 14:41:11 +0200

    Experimental server distance test

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,15 @@ const router = express.Router();
 let peers = [];
 
 router.post('/registerPeer', (req, res) => {
-    const { peerID, role} = req.body;
+    const { peerID, longitude, latitude, role} = req.body;
     if (!peerID || !role) {
         return res.status(400).json({ error: 'Peer ID is required' });
     }
     const existingPeer = peers.find(peer => peer.peerID === peerID);
-
     if (existingPeer) {
-        if (role === "stopIdle") {
-            existingPeer.role = "stopIdle";
-            return res.status(410).json({error: "Changed role to stopIdle"});
-        }
         return res.status(409).json({ error: 'Peer ID already registered' });
     }
-    peers.push({peerID, role});
+    peers.push({peerID, longitude, latitude, role});
     console.log(`Peer ID ${peerID} registered. Current list of peers:`, peers);
 
     res.json({ message: 'Peer registered successfully' });
